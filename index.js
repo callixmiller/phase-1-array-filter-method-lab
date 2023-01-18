ec51a4eb1445875593f929afb892e1f337cec796
callixmiller <calmill413@gmail.com>
2023-01-18 16:04:05 -0500

    index.js passing all tests

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution here
+function findMatching(drivers, string){
+    const result = drivers.filter(drivers => drivers.toLowerCase() === string.toLowerCase())
+    return result;
+} 
+
+function fuzzyMatch(drivers, string){
+    const result = drivers.filter(drivers => drivers.startsWith(string))
+    return result;
+}
+
+function matchName(drivers, string){
+    const result = drivers.filter(drivers => drivers.name === string)
+    return result;
+}
